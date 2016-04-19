eb482e03a832049b23b6dfe0c81ccc7c4bcedf8f
Adam Schwartz <PhantasyStar.Adam@gmail.com>
2016-04-18 20:49:43 -0700

    created table via js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Hello from the JavaScript file");
+
+function createTable(numCols, numRows) {
+  var begin = '<table class="theTable">';
+  var middle = "";
+  var end = "</table>";
+  for (var r = 0; r < numRows; r++) {
+    middle += '<tr class="row">';
+    for (var c = 0; c < numCols; c++) {
+      middle += '<td class="cell"></td>';
+    };
+    middle += "</tr>";
+  }
+  return begin + middle + end;
+}
+
+document.getElementsByTagName("body")[0].innerHTML = createTable(25, 15);
+
+// document.createElement("table");
+// var table = document.querySelector("table");
+// table.createElement("tr");
